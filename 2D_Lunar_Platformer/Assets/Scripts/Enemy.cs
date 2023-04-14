e1b2e6310505d016c8aafa8d1fec2144ee08a26d
Kamech97 <86726838+Kamech97@users.noreply.github.com>
2023-04-14 19:02:06 +0000

    Shyroe - Wave Manager
    Enemy Scripts + WaveSpawner Script + Game Scene created

---
+++ 2D_Lunar_Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health;
+    public int cost;
+    public float Speed;
+    public int damage;
+}
