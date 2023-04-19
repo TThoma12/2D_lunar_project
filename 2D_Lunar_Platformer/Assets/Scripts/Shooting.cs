955bd3e80dec7400e3d00fb387a3da06f260c656
Kamech97 <86726838+Kamech97@users.noreply.github.com>
2023-04-19 13:57:39 +0000

    Build

--- 2D_Lunar_Platformer/Assets/Scripts/Shooting.cs
+++ 2D_Lunar_Platformer/Assets/Scripts/Shooting.cs
@@ -14,7 +14,7 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && canShoot)
+        if (Input.GetKeyDown("space") && canShoot)
         {
             StartCoroutine(ShootGun());
         }
